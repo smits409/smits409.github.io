ac682f7da84665f4199f2b68f5167bd24e9225d0
smits409 <smits409@newschoo.edu>
2015-10-16 14:45:44 +0000

    javascript

---
+++ scripts.js
@@ -0,0 +1,11 @@
+console.log("is it cold outside?");
+console.log("type yes or no");
+
+
+var symbols = ["*","_","*","_","*","_","*","_","*","_","*",]
+var pattern = "snow";
+
+for (var increment = 2; increment > 1; increment++) {
+    console.log(pattern);
+    pattern = symbols[increment]; 
+      } 
