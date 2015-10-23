43ea0104ac9c55b0d1ee350e7ea9c1f2f2134c5c
smits409 <smits409@newschoo.edu>
2015-10-23 19:48:18 +0000

    friday

---
+++ exercise.js
@@ -0,0 +1,23 @@
+$("body").css("background-color","pink");
+$("h1").html("welcome home")
+
+$(".bigredbutton").click(function() {
+    console.log("you clicked it");
+    $("h1").css("background-color","white");
+}
+
+
+
+
+
+function myfunction () {
+    // do stuff
+    console.log ("hello");
+}
+
+function addNums(num1, num2) {
+    var sum = num1 + num2;
+    return sum;
+    
+    
+}
