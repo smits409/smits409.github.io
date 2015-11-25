c3463887335b07de03e3526d4178ebd02c34e98a
smits409 <smits409@newschoo.edu>
2015-11-25 02:32:11 +0000

    more stuff

--- realplant/page6.js
+++ realplant/page6.js
@@ -1,7 +1,7 @@
 
 $(function() {
     $(".sun").click(function() {
-        console.log("sun");
+        console.log("cloud");
         
         $(".plant5").css("display", "none");
         $(".plant3").css("display", "none");
@@ -13,16 +13,22 @@ $(function() {
         $(".cloud").css("display", "none");
         $(".cloud2").css("display", "block");
         
-        
         $(".food").css("display", "none");
+        $(".magic").css("display", "none");
+        $(".meat").css("display", "none");
+        
+        $(".small").css("display", "none");
+        $(".medium").css("display", "none");
+        $(".big").css("display", "none");
+        $(".chomp").css("display", "none");
     });
 });
 
 
 $( ".cloud2" )
   .click(function() {
-    $(".rain1").css("display", "block");
-    console.log("cloud");
+    
+    console.log("cloud2");
     
         $(".plant5").css("display", "none");
         $(".plant3").css("display", "none");
@@ -30,15 +36,47 @@ $( ".cloud2" )
         $(".plant1").css("display", "none");
         $(".pot").css("display", "none");
         
-        
         $(".cloud").css("display", "block");
+        $(".cloud2").css("display", "none");
+        $(".rain1").css("display", "block");
+       
         $(".food").css("display", "none");
+        $(".magic").css("display", "none");
+        $(".meat").css("display", "none");
+        
+        $(".small").css("display", "none");
+        $(".medium").css("display", "none");
+        $(".big").css("display", "none");
+        $(".chomp").css("display", "none");
   })
 
   
 $( ".cloud" )
   .click(function() {
-    $(".rain1").css("display", "block");
+      
+    console.log("plant3");
+    
+        $(".plant5").css("display", "none");
+        $(".plant3").css("display", "block");
+        $(".plant2").css("display", "none");
+        $(".plant1").css("display", "none");
+        $(".pot").css("display", "none");
+      
+        $(".rain1").css("display", "block");
+        $(".cloud2").css("display", "none");
+        
+        $(".food").css("display", "none");
+        $(".magic").css("display", "none");
+        $(".meat").css("display", "none");
+        
+        $(".small").css("display", "none");
+        $(".medium").css("display", "none");
+        $(".big").css("display", "none");
+        $(".chomp").css("display", "none");
+  })
+  
+$( ".plant3" )
+  .click(function() {
     console.log("can");
     
         $(".plant5").css("display", "none");
@@ -46,13 +84,21 @@ $( ".cloud" )
         $(".plant2").css("display", "none");
         $(".plant1").css("display", "none");
         $(".pot").css("display", "none");
-       
-        
-        $(".food").css("display", "block");
+      
         $(".rain1").css("display", "none");
         $(".cloud").css("display", "none");
         $(".cloud2").css("display", "none");
+        
+        $(".food").css("display", "block");
+        $(".magic").css("display", "none");
+        $(".meat").css("display", "none");
+        
+        $(".small").css("display", "none");
+        $(".medium").css("display", "none");
+        $(".big").css("display", "none");
+        $(".chomp").css("display", "none");
   })
+   
   
   
 $(function() {
@@ -69,6 +115,14 @@ $(function() {
         $(".cloud").css("display", "none");
         $(".cloud2").css("display", "none");
         
+        $(".magic").css("display", "none");
+        $(".meat").css("display", "none");
+        
+        $(".small").css("display", "none");
+        $(".medium").css("display", "none");
+        $(".big").css("display", "none");
+        $(".chomp").css("display", "none");
+        
     });
 });
  
@@ -77,14 +131,77 @@ $(function() {
     $(".plant5").click(function() {
         console.log("magic beans");
         
-        $(".magic").css("display", "block");
         $(".plant5").css("display", "block");
         $(".plant3").css("display", "none");
         $(".plant1").css("display", "none");
         $(".plant2").css("display", "none");
         $(".pot").css("display", "none");
         
+        $(".cloud").css("display", "none");
+        $(".cloud2").css("display", "none");
         $(".rain1").css("display", "none");
+        
+        $(".food").css("display", "none");
+        $(".magic").css("display", "block");
+        $(".meat").css("display", "none");
+        
+        $(".small").css("display", "none");
+        $(".medium").css("display", "none");
+        $(".big").css("display", "none");
+        $(".chomp").css("display", "none");
+        
+    });
+});
+ 
+ 
+$(function() {
+    $(".magic").click(function() {
+        console.log("growing");
+        
+        $(".plant5").css("display", "none");
+        $(".plant3").css("display", "none");
+        $(".plant1").css("display", "none");
+        $(".plant2").css("display", "none");
+        $(".pot").css("display", "none");
+        
+        $(".cloud").css("display", "none");
+        $(".cloud2").css("display", "none");
+        $(".rain1").css("display", "none");
+        
+        $(".food").css("display", "none");
+        $(".magic").css("display", "none");
+        $(".meat").css("display", "none");
+        
+        $(".small").css("display", "block");
+        $(".medium").css("display", "none");
+        $(".big").css("display", "none");
+        $(".chomp").css("display", "none");
+        
+    });
+});
+
+$(function() {
+    $(".meat").click(function() {
+        console.log("growing");
+        
+        $(".plant5").css("display", "none");
+        $(".plant3").css("display", "none");
+        $(".plant1").css("display", "none");
+        $(".plant2").css("display", "none");
+        $(".pot").css("display", "none");
+        
+        $(".cloud").css("display", "none");
+        $(".cloud2").css("display", "none");
+        $(".rain1").css("display", "none");
+        
+        $(".food").css("display", "none");
+        $(".magic").css("display", "none");
+        $(".meat").css("display", "none");
+        
+        $(".small").css("display", "none");
+        $(".medium").css("display", "none");
+        $(".big").css("display", "none");
+        $(".chomp").css("display", "none");
         
     });
 });
