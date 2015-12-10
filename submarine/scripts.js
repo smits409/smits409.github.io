806b10ecaa06799ac0415e1811e89c4aee3f7a8d
smits409 <smits409@newschoo.edu>
2015-12-10 01:36:03 +0000

    index stuff

--- submarine/scripts.js
+++ submarine/scripts.js
@@ -1,16 +1,14 @@
 
-$(function() {
     $(".on").click(function() {
         console.log("you hit the switch off!");
         
         $(".off").css("display", "block");
         $(".on").css("display", "none");
         $(".lightsoff").css("display", "block");
-        
+        var audio = new Audio('dog.mp3');
+audio.play();
     });
-});
 
-$(function() {
     $(".off").click(function() {
         console.log("you hit the switch on!");
         
@@ -19,4 +17,4 @@ $(function() {
         $(".lightsoff").css("display", "none");
         
     });
-});
+
