660ccf59c6302f560fc21a061e6efbafa8902550
smits409 <smits409@newschoo.edu>
2015-12-11 23:06:54 +0000

    new stuff

---
+++ submarine/scripts1.js
@@ -0,0 +1,14 @@
+
+    $(".jordanna").click(function() {
+        console.log("jordanna");
+        
+        
+    });
+    
+    
+    
+    $(".oliver").click(function() {
+        console.log("oliver");
+        
+        
+    });
