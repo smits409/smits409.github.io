66746800b3a2b99e8f361cd5dc674385bae9e014
sage <smits409@newschool.edu>
2016-02-24 21:45:33 -0500

    new space page with separate divs

--- space/assets/js/app.js
+++ space/assets/js/app.js
@@ -7,18 +7,42 @@
         $(".coatfolded").css("display", "block");
     });
 
+        $(".coatfolded").click(function() {
+        console.log("you folded the coat!");
+        
+        $(".coat").css("display", "block");
+        $(".coatfolded").css("display", "none");
+    });
+
+
+
     $(".blanket").click(function() {
         console.log("you folded the blanket!");
         
         $(".blanket").css("display", "none");
         $(".blanketfolded").css("display", "block");
     });
+        $(".blanketfolded").click(function() {
+        console.log("you folded the blanket!");
+        
+        $(".blanket").css("display", "block");
+        $(".blanketfolded").css("display", "none");
+    });
+
+
 
     $(".candle").click(function() {
         console.log("you lit the candle!");
         
         $(".candle").css("display", "none");
         $(".candlelit").css("display", "block");
+    });
+
+        $(".candlelit").click(function() {
+        console.log("you lit the candle!");
+        
+        $(".candle").css("display", "block");
+        $(".candlelit").css("display", "none");
     });
 
 
