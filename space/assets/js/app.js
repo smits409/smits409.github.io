38bcd4cd606b4f1f57cbf691b572023b617cad30
sage <smits409@newschool.edu>
2016-03-04 11:42:15 -0500

    adding stuff to space

--- space/assets/js/app.js
+++ space/assets/js/app.js
@@ -24,91 +24,32 @@
 
 
 
-    $(".first").hover(function() {
-        console.log("you hovered the first image"); 
-        $(".text2").css("display", "block");
-    });
-
-
-    $(".second").hover(function() {
-        console.log("you hovered over nothing"); 
-        $(".text2").css("display", "none");
-        $(".text3").css("display", "none");
-        $(".text4").css("display", "none");
-        $(".text5").css("display", "none");
-    });
-
-    $(".third").hover(function() {
-        console.log("you hovered the candle"); 
-        $(".text3").css("display", "block");
-        $(".text4").css("display", "none");
-        $(".text5").css("display", "none");
-    });
-
-     $(".fourth").hover(function() {
-        console.log("you hovered the bed"); 
-        $(".text4").css("display", "block");
-        $(".text2").css("display", "none");
-        $(".text3").css("display", "none");
-        $(".text5").css("display", "none");
-    });
-
-    $(".fifth").hover(function() {
-        console.log("you hovered the coat"); 
-        $(".text5").css("display", "block");
-        $(".text2").css("display", "none");
-        $(".text3").css("display", "none");
-        $(".text4").css("display", "none");
-    });
-
-    $(".sixth").hover(function() {
-        console.log("you hovered nothing"); 
-        $(".text5").css("display", "none");
-        $(".text2").css("display", "none");
-        $(".text3").css("display", "none");
-        $(".text4").css("display", "none");
-    });
-
-
-
 
     $(".circle1").hover(function() {
         console.log("you hovered the circle"); 
-        $(".circle1").css("background", "#274F7D");
+        $(".circle1").css("background", "#1E4378");
     });
 
     $(".circle2").hover(function() {
         console.log("you hovered the circle"); 
-        $(".circle2").css("background", "#274F7D");
+        $(".circle2").css("background", "#1E4378");
     });
 
     $(".circle3").hover(function() {
         console.log("you hovered the circle"); 
-        $(".circle3").css("background", "#274F7D");
+        $(".circle3").css("background", "#1E4378");
     });
 
     $(".circle4").hover(function() {
         console.log("you hovered the circle"); 
-        $(".circle4").css("background", "#274F7D");
+        $(".circle4").css("background", "#1E4378");
     });
 
 
 
 
-    $(".circle11").hover(function() {
-        console.log("you hovered the circle"); 
-        $(".circle11").css("background", "#274F7D");
-    });
 
-    $(".circle22").hover(function() {
-        console.log("you hovered the circle"); 
-        $(".circle22").css("background", "#274F7D");
-    });
 
-    $(".circle33").hover(function() {
-        console.log("you hovered the circle"); 
-        $(".circle33").css("background", "#274F7D");
-    });
 
 
 
