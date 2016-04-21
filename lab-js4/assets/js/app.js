426d9e5877a6c70e47aa05f47714e89cea90b97a
sage <smits409@newschool.edu>
2016-04-21 12:04:31 -0400

    classwork

--- lab-js4/assets/js/app.js
+++ lab-js4/assets/js/app.js
@@ -1,37 +1,66 @@
 
 
-var objects = ['assets/img/bunny.png',
-				'assets/img/cat.jpg',
-				'assets/img/dog.png',
-				'assets/img/sloth.png',
-				'assets/img/circle.png',
-				'assets/img/square.png',
-				'assets/img/triangle.png',
-				'Meow', 'Woof','...','Meep'
-				];
+var objects = ['assets/img/blue1.png',
+				'assets/img/blue2.png',
+				'assets/img/blue3.png',
+				'assets/img/blue4.png',
+				'assets/img/blue5.png',
+			  ];
 
-var colors = ['red', 'blue', 'pink'];
+var objects2 = ['assets/img/pink1.png',
+				'assets/img/pink2.png',
+				'assets/img/pink3.png',
+				'assets/img/pink4.png',
+				'assets/img/pink5.png',
+			  ];
+
+var objects3 = ['assets/img/yellow1.png',
+				'assets/img/yellow2.png',
+				'assets/img/yellow3.png',
+				'assets/img/yellow4.png',
+				'assets/img/yellow5.png'
+			  ];
+
 
 
 $('.button').click(function() {
 	var random_object_number 	 = Math.floor(Math.random() * objects.length),
-		random_color_number		 = Math.floor(Math.random() * colors.length),
 		object_to_use			 = objects[random_object_number],
-		color_to_use			 = colors[random_color_number],
-		result 					 = '';
-
-
-
-	if (random_object_number > 6) {
-		result = '<span class="object ' + color_to_use + '">' + object_to_use + '</span>';	
-	} else {
-		result = '<img class="' + color_to_use + '" src="' + object_to_use + '"/>';
-	}
-
-
+		result = '<img src="' + object_to_use + '"/>';
+	
 
 	$('.content').prepend(result);
 
 	//$('.content').append(objects[random_object]);
 	//$('.content').append('<img src="assets/img/bunny.png"/>');
 });
+
+$('.button2').click(function() {
+	var random_object_number 	 = Math.floor(Math.random() * objects2.length),
+		object_to_use			 = objects2[random_object_number],
+		result = '<img src="' + object_to_use + '"/>';
+	
+
+	$('.content').prepend(result);
+
+	//$('.content').append(objects[random_object]);
+	//$('.content').append('<img src="assets/img/bunny.png"/>');
+});
+
+$('.button3').click(function() {
+	var random_object_number 	 = Math.floor(Math.random() * objects3.length),
+		object_to_use			 = objects3[random_object_number],
+		result = '<img src="' + object_to_use + '"/>';
+	
+
+	$('.content').prepend(result);
+
+	//$('.content').append(objects[random_object]);
+	//$('.content').append('<img src="assets/img/bunny.png"/>');
+});
+
+
+
+
+
+
